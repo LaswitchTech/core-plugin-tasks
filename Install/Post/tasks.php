267a13e171916116e6671cb0cc0481637f6d0131
Louis Ouellet <louis_ouellet@hotmail.com>
2025-10-16 11:52:12 -0400

    General: Updated the database schematics

--- Install/Post/tasks.php
+++ Install/Post/tasks.php
@@ -12,6 +12,9 @@ class TasksPostModel extends TasksModel {
      */
     public function post($record): array
     {
+        // Check if the record ID is below 9999
+        if($record['id'] <= 9999) return $record;
+
         // Loop through the record
         foreach($record as $key => $value){
 
